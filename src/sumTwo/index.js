69f060e5993978f7177feb3b881b8ccb39ebdaa1
developerbello <developerbello@gmail.com>
2021-12-22 23:16:40 +0100

    implement two-sum solutions (using brute-force, one pass and two pass aproach)

--- src/sumTwo/index.js
+++ src/sumTwo/index.js
@@ -1,18 +1,43 @@
+// Brute force approach
+
 const twoSum = (list, target) => {
-
   for (let i = 0; i < list.length; i++) {
-    const first = list[i];
-    
-    for (let j = 0; j < list.length; j++) {
-      const second = list[j];
-      if (i === j ) continue;
-
-      const sum = first + second;
-      if ( sum === target ) {
+    for (let j = i + 1; j < list.length; j++) {
+      if (list[i] + list[j] === target) {
         return [i, j];
       }
     }
   }
-}
+};
 
-export default twoSum;
+const twoSum2 = (list, target) => {
+  let hashMap = {};
+
+  for (let i = 0; i < list.length; i++) {
+    hashMap[list[i]] = i;
+  }
+  for (let i = 0; i < list.length; i++) {
+    const complement = target - list[i];
+
+    if (complement in hashMap && hashMap[complement] !== i) {
+      return [i, hashMap[complement]];
+    }
+  }
+};
+
+const twoSum3 = (list, target) => {
+  let hashMap = {};
+
+  for (let i = 0; i < list.length; i++) {
+    let complement = target - list[i]; // 10 - 2 = 8
+
+    if (complement in hashMap) {
+      return [hashMap[complement], i];
+    }
+    hashMap[list[i]] = i;
+  }
+};
+
+twoSum3([2, 3, 5, 7, 8], 7);
+
+export { twoSum, twoSum2, twoSum3 };
