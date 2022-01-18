50368b52349bd6765f199da8912f47fcccf14c48
belloibrav@gmail.com <developerbello@gmail.com>
2022-01-18 04:18:23 +0100

    Solution to twoSum algorithm
    The foolwing approaches were used to solve the problem
    - Brute force approach : direct way to write the soltion (non optimized soltion)
    - Two pass approach : time-space complexities is O(2N) which literally translate as O(N) optimal when remove coeffiecient or constant value which is 2.
    - One pass approach : time-space complexities is O(N) more optimal.

---
+++ src/twoSum/main.js
@@ -0,0 +1,10 @@
+import twoSum from ".";
+
+const firstResult = twoSum([1,2,3,4,5], 5);
+console.log(firstResult);
+const secondResult = twoSum([2,7,11,15], 9);
+console.log(secondResult);
+const thirdResult = twoSum([3,2,4], 6);
+console.log(thirdResult);
+const fourthResult = twoSum([3,3], 6);
+console.log(fourthResult);
